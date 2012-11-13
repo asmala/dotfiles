9ab4b2fe9cdaebb038670471ba51c9be99f31c7d
Janne Asmala <janne@asmala.name>
2012-11-14 07:33:10 +0800

    Added Midje plugin.

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:dependencies [[org.clojure/tools.nrepl "0.2.0-beta9"]]
-        :plugins [[lein-swank "1.4.4"]]}}
+        :plugins [[lein-midje "2.0.0-SNAPSHOT"]
+                  [lein-swank "1.4.4"]]}}
